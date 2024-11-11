02291437836480faca19234bc9397212dd65ca09
lukew7375 <lukew7375@gmail.com>
2024-11-10 20:07:57 -0500

    Added the viper slide to the RUN_TO_POSITION function

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriveViperSlide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriveViperSlide.java
@@ -27,23 +27,28 @@ public class MecanumDriveViperSlide extends OpMode {
 
     final double ARM_POWER = 1d;
     final double VIPER_SLIDE_POWER = 0.5d;
-    //viper slide positions: low basket: 0; high basket: -1540; sumbersible: -1330 (approx); collect off ground: -302; high rung: 0
-    final double ARM_TICKS_PER_DEGREE = 7.46805555555; // taken from GoBilda for test arm
+
+    final int VS_GROUND = -302;
+    final int VS_SUBMERSIBLE = -1330;
+    final int VS_HIGH_RUNG = 0;
+    final int VS_LOW_BASKET = 0;
+    final int VS_HIGH_BASKET = -1540;
+    final int[] VS_POSITIONS = {VS_GROUND, VS_SUBMERSIBLE, VS_HIGH_RUNG, VS_LOW_BASKET, VS_HIGH_BASKET};
+    int vsPos = 0;
+    final double ARM_TICKS_PER_DEGREE = 7.46805555555;
     final double ORIGIN = 0 * ARM_TICKS_PER_DEGREE;
+    final double ARM_SUBMERSIBLE = 20 * ARM_TICKS_PER_DEGREE;
+    final double ARM_HIGH_RUNG = 75 * ARM_TICKS_PER_DEGREE;
+    final double ARM_LOW_BASKET = 80 * ARM_TICKS_PER_DEGREE;
+    final double ARM_HIGH_BASKET = 100 * ARM_TICKS_PER_DEGREE;
+    final double[] ARM_POSITIONS = {ORIGIN, ARM_SUBMERSIBLE, ARM_HIGH_RUNG, ARM_LOW_BASKET, ARM_HIGH_BASKET}; // arm positions array to cycle through
+    double armPos = 0; // the variable which the arm motor position will be set to
+    int posIdx = 0; // variable to track what index of ARM_POSITIONS is being used
 
-    final double SUBMERSIBLE = 20 * ARM_TICKS_PER_DEGREE;
-    final double HIGH_RUNG = 75 * ARM_TICKS_PER_DEGREE;
-    final double LOW_BASKET = 80 * ARM_TICKS_PER_DEGREE;
-    final double HIGH_BASKET = 100 * ARM_TICKS_PER_DEGREE;
-    final double[] ARM_POSITIONS = {ORIGIN, SUBMERSIBLE, HIGH_RUNG, LOW_BASKET, HIGH_BASKET}; // test array to cycle through
-    double armPos = 0; // creating and initializing the variable which the arm motor position will be set to
-    int armPosIdx = 0; // variable to track what index of ARM_POSITIONS is being used
-
-    // variables telling whether or not the arm can be moved foreward or backward
+    // variables telling whether or not the arm can be moved forward or backward
     boolean armForward = true;
     boolean armBackward = true; 
 
-    // TODO: CREATE CONSTANTS FOR POSITIONS FOR SERVOS
     final double CLAW_CLOSED = 0d;
     final double CLAW_OPEN = 0.3d;
 
@@ -82,42 +87,33 @@ public class MecanumDriveViperSlide extends OpMode {
 
          // Setting up the test arm motor to RUN_TO_POSITION
          armMotor2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-         armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+         armMotor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-         // TODO: Set servos initial position
-        wrist.setPosition(WRIST_IN);
-        claw.setPosition(CLAW_CLOSED);
-        activeIntake.setPower(0);
+         viperSlideMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+         wrist.setPosition(WRIST_IN);
+         claw.setPosition(CLAW_CLOSED);
+         activeIntake.setPower(0);
     }
 
     @Override
     public void loop() {
 
-        
-        /* commented out for testing
-        if(gamepad1.dpad_up)
-            armMotor.setPower(ARM_POWER);
-        else if (gamepad1.dpad_down)
-            armMotor.setPower(ARM_POWER * -1);
-        else
-            armMotor.setPower(0); */
-
-        // test arm movements
         if(gamepad1.dpad_up && armForward && !gamepad1.dpad_down)
             {
                 armForward = false;
                 armBackward = true;
-                armPosIdx = armPosIdx - 1; 
+                posIdx = posIdx - 1;
                 
                 // added to account for setting armPosIdx to -1
-                if(armPosIdx < 0)
-                    armPosIdx = ARM_POSITIONS.length - 1;
+                if(posIdx < 0)
+                    posIdx = ARM_POSITIONS.length - 1;
             }
         else if(gamepad1.dpad_down && armBackward && !gamepad1.dpad_up)
             {
                 armForward = true;
                 armBackward = false;
-                armPosIdx = (armPosIdx + 1) % ARM_POSITIONS.length;
+                posIdx = (posIdx + 1) % ARM_POSITIONS.length;
             }
         else if( !gamepad1.dpad_up && !gamepad1.dpad_down )
             {
@@ -125,23 +121,31 @@ public class MecanumDriveViperSlide extends OpMode {
                 armForward = true;
             }
 
-        armPos = ARM_POSITIONS[ armPosIdx ];
+        armPos = ARM_POSITIONS[ posIdx ];
+        vsPos = VS_POSITIONS[ posIdx ];
 
         armMotor2.setTargetPosition((int)armPos);
         armMotor2.setPower(0.3);
         armMotor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-        telemetry.addData( "Arm Motor Position", armMotor2.getCurrentPosition() / ARM_TICKS_PER_DEGREE);
+        viperSlideMotor.setTargetPosition(vsPos);
+        viperSlideMotor.setPower(0.8);
+        viperSlideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-        if(gamepad1.x)
+        if(gamepad1.x) {
+            viperSlideMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
             viperSlideMotor.setPower(VIPER_SLIDE_POWER);
-        else if (gamepad1.y)
+        }
+        else if (gamepad1.y) {
+            viperSlideMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
             viperSlideMotor.setPower(VIPER_SLIDE_POWER * -1);
+        }
         else
             viperSlideMotor.setPower(0);
 
-        telemetry.addData( "Viper Slide Motor Position", viperSlideMotor.getCurrentPosition());
-        telemetry.update();
+        // possibly necessary:
+        // viperSlideMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
 
         double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
         double x = gamepad1.right_stick_x * 1.1; // Counteract imperfect strafing
@@ -162,10 +166,9 @@ public class MecanumDriveViperSlide extends OpMode {
         backRightMotor.setPower(backRightPower);
 
         // SERVOS
+        // ACTIVE INTAKE - In dpad_down - Out dpad_up
         // CLAW - Open A Close B
-        // WRIST - Open X - Close Y (Increment by CONSTANT
-
-        // TODO: Handle servo controls with gamepad
+        // WRIST - Open X - Close Y
         if(gamepad2.dpad_down)
             activeIntake.setPower(INTAKE_IN);
         else if(gamepad2.dpad_up)
@@ -183,6 +186,9 @@ public class MecanumDriveViperSlide extends OpMode {
         else if (gamepad2.y)
             wrist.setPosition(WRIST_IN);
 
+        telemetry.addData( "Arm Motor Position", armMotor2.getCurrentPosition() / ARM_TICKS_PER_DEGREE);
+        telemetry.addData( "Viper Slide Motor Position", viperSlideMotor.getCurrentPosition());
+        telemetry.update();
 
     }
 
