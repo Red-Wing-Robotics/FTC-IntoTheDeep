f899c6c2f79f2d1d1dc43f7cf9426996af5b51d1
David Tucker <david@davidtucker.net>
2024-11-24 09:26:07 -0500

    Meeting work - Luke W.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriveViperSlide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriveViperSlide.java
@@ -13,7 +13,7 @@ public class MecanumDriveViperSlide extends OpMode {
     // Setup Servos
     //    Claw Expansion Hub 1 "claw"
     //    Wrist Expansion Hub 0 "wrist"
-    Servo wrist  = null;
+    Servo wrist = null;
     Servo claw = null;
     CRServo activeIntake = null;
 
@@ -25,7 +25,7 @@ public class MecanumDriveViperSlide extends OpMode {
     public DcMotor armMotor = null;
     DcMotor armMotor2 = null;
 
-    final double ARM_POWER = 0.3d;
+    final double ARM_POWER = 1d;
     final double VIPER_SLIDE_POWER = 1d;
 
     final int VS_GROUND = -302;
@@ -46,7 +46,7 @@ public class MecanumDriveViperSlide extends OpMode {
 
     // variables telling whether or not the arm can be moved forward or backward
     boolean armForward = true;
-    boolean armBackward = true; 
+    boolean armBackward = true;
 
     final double CLAW_CLOSED = 0d;
     final double CLAW_OPEN = 0.3d;
@@ -61,41 +61,41 @@ public class MecanumDriveViperSlide extends OpMode {
     @Override
     public void init() {
         // initializing servos
-         wrist = hardwareMap.get(Servo .class, "wrist");
-         claw = hardwareMap.get(Servo .class, "claw");
-         activeIntake = hardwareMap.get(CRServo .class, "active intake");
+        wrist = hardwareMap.get(Servo.class, "wrist");
+        claw = hardwareMap.get(Servo.class, "claw");
+        activeIntake = hardwareMap.get(CRServo.class, "active intake");
 
-         // initializing motors
-         frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
-         backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
-         frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
-         backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
-         viperSlideMotor = hardwareMap.get(DcMotor.class, "viperSlideMotor");
-         armMotor = hardwareMap.get(DcMotor.class, "left_arm");
-         armMotor2 = hardwareMap.get(DcMotor.class, "arm2"); // the 2 outlet in the expansion hub will be the test motor
+        // initializing motors
+        frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
+        backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
+        frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
+        backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
+        viperSlideMotor = hardwareMap.get(DcMotor.class, "viperSlideMotor");
+        armMotor = hardwareMap.get(DcMotor.class, "left_arm");
+        armMotor2 = hardwareMap.get(DcMotor.class, "arm2"); // the 2 outlet in the expansion hub will be the test motor
 
-         // set behavior flags for hardware
-         armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-         frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-         frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-         backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-         backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        // set behavior flags for hardware
+        armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-         frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-         backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
-         // Setting up the test arm motor to RUN_TO_POSITION
-         armMotor2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-         armMotor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        // Setting up the test arm motor to RUN_TO_POSITION
+        armMotor2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        armMotor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-         // viperSlideMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        // viperSlideMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-         wrist.setPosition(WRIST_IN);
-         claw.setPosition(CLAW_CLOSED);
-         activeIntake.setPower(0);
+        wrist.setPosition(WRIST_IN);
+        claw.setPosition(CLAW_CLOSED);
+        activeIntake.setPower(0);
     }
 
-    public void setMotorPosition( DcMotor motor, int pos, double power ){
+    public void setMotorPosition(DcMotor motor, int pos, double power) {
         motor.setTargetPosition(pos);
         motor.setPower(power);
         motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -104,57 +104,77 @@ public class MecanumDriveViperSlide extends OpMode {
     @Override
     public void loop() {
 
+        // Drive Functionality
+        mecanumTankDrive();
+
         viperSlideMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        if(gamepad1.dpad_up && armForward && !gamepad1.dpad_down)
-            {
-                armForward = false;
-                armBackward = true;
-                posIdx = posIdx - 1;
-                
-                // added to account for setting armPosIdx to -1
-                if(posIdx < 0)
-                    posIdx = ARM_POSITIONS.length - 1;
+        if (gamepad1.dpad_up && armForward && !gamepad1.dpad_down) {
+            armForward = false;
+            armBackward = true;
+            posIdx = posIdx - 1;
 
-                // armPos = ARM_POSITIONS[ posIdx ];
+            // added to account for setting armPosIdx to -1
+            if (posIdx < 0)
+                posIdx = ARM_POSITIONS.length - 1;
 
-                // setMotorPosition( armMotor2, (int)armPos, ARM_POWER );
-                // setMotorPosition( viperSlideMotor, VS_POSITIONS[ posIdx ], VIPER_SLIDE_POWER);
-            }
-        else if(gamepad1.dpad_down && armBackward && !gamepad1.dpad_up)
-            {
-                armForward = true;
-                armBackward = false;
-                posIdx = (posIdx + 1) % ARM_POSITIONS.length;
+            // armPos = ARM_POSITIONS[ posIdx ];
 
-                // armPos = ARM_POSITIONS[ posIdx ];
+            // setMotorPosition( armMotor2, (int)armPos, ARM_POWER );
+            // setMotorPosition( viperSlideMotor, VS_POSITIONS[ posIdx ], VIPER_SLIDE_POWER);
+        } else if (gamepad1.dpad_down && armBackward && !gamepad1.dpad_up) {
+            armForward = true;
+            armBackward = false;
+            posIdx = (posIdx + 1) % ARM_POSITIONS.length;
 
-                // setMotorPosition( armMotor2, (int)armPos, ARM_POWER );
-                // setMotorPosition( viperSlideMotor, VS_POSITIONS[ posIdx ], VIPER_SLIDE_POWER);
-            }
-        else if( !gamepad1.dpad_up && !gamepad1.dpad_down )
-            {
-                armBackward = true;
-                armForward = true;
-            }
-        armPos = ARM_POSITIONS[ posIdx ];
-        setMotorPosition(armMotor2, (int)armPos, ARM_POWER);
+            // armPos = ARM_POSITIONS[ posIdx ];
 
-        if(gamepad1.x) {
+            // setMotorPosition( armMotor2, (int)armPos, ARM_POWER );
+            // setMotorPosition( viperSlideMotor, VS_POSITIONS[ posIdx ], VIPER_SLIDE_POWER);
+        } else if (!gamepad1.dpad_up && !gamepad1.dpad_down) {
+            armBackward = true;
+            armForward = true;
+        }
+        armPos = ARM_POSITIONS[posIdx];
+        setMotorPosition(armMotor2, (int) armPos, ARM_POWER);
+
+        if (gamepad1.x) {
             // viperSlideMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
             viperSlideMotor.setPower(VIPER_SLIDE_POWER);
-        }
-        else if (gamepad1.y) {
+        } else if (gamepad1.y) {
             // viperSlideMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
             viperSlideMotor.setPower(VIPER_SLIDE_POWER * -1);
-        }
-        else
+        } else
             viperSlideMotor.setPower(0);
 
-        // possibly necessary:
-        // viperSlideMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        // SERVOS
+        // ACTIVE INTAKE - In dpad_down - Out dpad_up
+        // CLAW - Open A Close B
+        // WRIST - Open X - Close Y
+        if (gamepad2.dpad_down)
+            activeIntake.setPower(INTAKE_IN);
+        else if (gamepad2.dpad_up)
+            activeIntake.setPower(INTAKE_OUT);
+        else
+            activeIntake.setPower(0);
 
+        if (gamepad2.a)
+            claw.setPosition(CLAW_OPEN);
+        else if (gamepad2.b)
+            claw.setPosition(CLAW_CLOSED);
 
+        if (gamepad2.x)
+            wrist.setPosition(WRIST_OUT);
+        else if (gamepad2.y)
+            wrist.setPosition(WRIST_IN);
+
+        telemetry.addData("Arm Motor Position", armMotor2.getCurrentPosition() / ARM_TICKS_PER_DEGREE);
+        telemetry.addData("Viper Slide Motor Position", viperSlideMotor.getCurrentPosition());
+        telemetry.update();
+
+    }
+
+    private void mecanumTankDrive() {
         double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
         double x = gamepad1.right_stick_x * 1.1; // Counteract imperfect strafing
         double rx = gamepad1.left_stick_x;
@@ -172,32 +192,6 @@ public class MecanumDriveViperSlide extends OpMode {
         backLeftMotor.setPower(backLeftPower);
         frontRightMotor.setPower(frontRightPower);
         backRightMotor.setPower(backRightPower);
-
-        // SERVOS
-        // ACTIVE INTAKE - In dpad_down - Out dpad_up
-        // CLAW - Open A Close B
-        // WRIST - Open X - Close Y
-        if(gamepad2.dpad_down)
-            activeIntake.setPower(INTAKE_IN);
-        else if(gamepad2.dpad_up)
-            activeIntake.setPower(INTAKE_OUT);
-        else
-            activeIntake.setPower(0);
-            
-        if(gamepad2.a)
-            claw.setPosition(CLAW_OPEN);
-        else if(gamepad2.b)
-            claw.setPosition(CLAW_CLOSED);
-
-        if(gamepad2.x)
-            wrist.setPosition(WRIST_OUT);
-        else if (gamepad2.y)
-            wrist.setPosition(WRIST_IN);
-
-        telemetry.addData( "Arm Motor Position", armMotor2.getCurrentPosition() / ARM_TICKS_PER_DEGREE);
-        telemetry.addData( "Viper Slide Motor Position", viperSlideMotor.getCurrentPosition());
-        telemetry.update();
-
     }
 
 }
