8dcb36813e0b2a0e53188845fb790115dbb60027
David Tucker <david@davidtucker.net>
2024-12-22 14:47:20 -0500

    Organizing main opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriveViperSlide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriveViperSlide.java
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -11,32 +10,32 @@ import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp(name = "Mecanum Viper Slide")
 public class MecanumDriveViperSlide extends OpMode {
 
-    // Setup Servos
-    //    Claw Expansion Hub 1 "claw"
-    //    Wrist Expansion Hub 0 "wrist"
+    //---------------------------------------------
+    // HARDWARE
+    //---------------------------------------------
+
+    // SERVOS
+
     Servo wrist = null;
     Servo claw = null;
-    CRServo activeIntake = null;
+
+    // MOTORS
 
     DcMotor frontLeftMotor = null;
     DcMotor backLeftMotor = null;
     DcMotor frontRightMotor = null;
     DcMotor backRightMotor = null;
-    public DcMotor viperSlideMotor = null;
-    public DcMotor armMotor = null;
+    DcMotor viperSlideMotor = null;
     DcMotor armMotor2 = null;
+
+    //---------------------------------------------
+    // CONSTANTS
+    //---------------------------------------------
+
+    // ARM ----------------------------------------
 
     final double ARM_POWER = 1d;
     final double VIPER_SLIDE_POWER = 1d;
-
-    /*
-    final int VS_GROUND = -302;
-    final int VS_SUBMERSIBLE = -1330;
-    final int VS_HIGH_RUNG = 0;
-    final int VS_LOW_BASKET = 0;
-    final int VS_HIGH_BASKET = -1540;
-    final int[] VS_POSITIONS = {VS_GROUND, VS_SUBMERSIBLE, VS_HIGH_RUNG, VS_LOW_BASKET, VS_HIGH_BASKET};
-    */
     final double ARM_TICKS_PER_DEGREE312 = 7.46805555555;
     final double ARM_TICKS_PER_DEGREE = 19.791666666667;
     final double ORIGIN = 0 * ARM_TICKS_PER_DEGREE;
@@ -49,6 +48,23 @@ public class MecanumDriveViperSlide extends OpMode {
 
     final double FUDGE_FACTOR = 5 * ARM_TICKS_PER_DEGREE;
 
+    // CLAW ---------------------------------------
+
+    final double CLAW_CLOSED = 0.3d;
+    final double CLAW_OPEN = 1d;
+
+    // WRIST --------------------------------------
+
+    final double WRIST_IN = 0.1d; //Controlled by y
+    final double WRIST_MID = 0.45d;
+    final double WRIST_OUT = 0.85d; //controlled by x
+
+    //---------------------------------------------
+    // LOCAL VARIABLES
+    //---------------------------------------------
+
+    // ARM ----------------------------------------
+
     double armPos = 0; // the variable which the arm motor position will be set to
     int posIdx = 0; // variable to track what index of ARM_POSITIONS is being used
 
@@ -56,24 +72,17 @@ public class MecanumDriveViperSlide extends OpMode {
     boolean armForward = true;
     boolean armBackward = true;
 
-    final double CLAW_CLOSED = 0.3d;
-    final double CLAW_OPEN = 1d;
-
-    final double WRIST_IN = 0.1d; //Controlled by y
-    final double WRIST_MID = 0.45d;
-    final double WRIST_OUT = 0.85d; //controlled by x
-
-    final double INTAKE_IN = -1d;
-    final double INTAKE_OUT = -INTAKE_IN;
-
     double armPositionFudgeFactor = 0.0d;
+
+    //---------------------------------------------
+    // OPMODE OVERRIDDEN METHODS
+    //---------------------------------------------
 
     @Override
     public void init() {
         // initializing servos
         wrist = hardwareMap.get(Servo.class, "wrist");
         claw = hardwareMap.get(Servo.class, "claw");
-        activeIntake = hardwareMap.get(CRServo.class, "active intake");
 
         // initializing motors
         frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
@@ -90,6 +99,7 @@ public class MecanumDriveViperSlide extends OpMode {
         backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
+        // Set direction for drive
         frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
@@ -101,23 +111,60 @@ public class MecanumDriveViperSlide extends OpMode {
 
         wrist.setPosition(WRIST_IN);
         claw.setPosition(CLAW_CLOSED);
-        activeIntake.setPower(0);
     }
 
-    public void setMotorPosition(DcMotor motor, int pos, double power) {
+    @Override
+    public void loop() {
+        // Drive Functionality
+        // GAMEPAD 1 : Left and Right Stick
+        mecanumTankDrive();
+
+        // Arm Functionality
+        // GAMEPAD 1 : DPAD Up and Down, L,R Bottom Trigger
+        controlArm();
+
+        // Viper Slide Functionality
+        // GAMEPAD 1 : X,Y
+        controlViperSlide();
+
+        // Servo Functionality
+        // GAMEPAD 2 : X,Y,A,B
+        controlServos();
+
+        telemetry.update();
+    }
+
+    //---------------------------------------------
+    // PRIVATE IMPLEMENTATION METHODS
+    //---------------------------------------------
+
+    private void setMotorPosition(DcMotor motor, int pos, double power) {
         motor.setTargetPosition(pos);
         motor.setPower(power);
         motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
 
-    @Override
-    public void loop() {
+    private void mecanumTankDrive() {
+        double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+        double x = gamepad1.right_stick_x * 1.1; // Counteract imperfect strafing
+        double rx = gamepad1.left_stick_x;
 
-        // Drive Functionality
-        mecanumTankDrive();
+        // Denominator is the largest motor power (absolute value) or 1
+        // This ensures all the powers maintain the same ratio,
+        // but only if at least one is out of the range [-1, 1]
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        double frontLeftPower = (y + x + rx) / denominator;
+        double backLeftPower = (y - x + rx) / denominator;
+        double frontRightPower = (y - x - rx) / denominator;
+        double backRightPower = (y + x - rx) / denominator;
 
-        viperSlideMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontLeftMotor.setPower(frontLeftPower);
+        backLeftMotor.setPower(backLeftPower);
+        frontRightMotor.setPower(frontRightPower);
+        backRightMotor.setPower(backRightPower);
+    }
 
+    private void controlArm() {
         if (gamepad1.dpad_up && armForward && !gamepad1.dpad_down) {
             armForward = false;
             armBackward = true;
@@ -148,6 +195,11 @@ public class MecanumDriveViperSlide extends OpMode {
 
         armPositionFudgeFactor = FUDGE_FACTOR * (gamepad1.right_trigger + (-gamepad1.left_trigger));
         setMotorPosition(armMotor2, (int) (armPos  + armPositionFudgeFactor), ARM_POWER);
+        telemetry.addData("Arm Motor Position", armMotor2.getCurrentPosition() / ARM_TICKS_PER_DEGREE);
+    }
+
+    private void controlViperSlide() {
+        viperSlideMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         boolean canExtend = (viperSlideMotor.getCurrentPosition() > -1235 || armMotor2.getCurrentPosition() > 74);
 
@@ -157,20 +209,14 @@ public class MecanumDriveViperSlide extends OpMode {
         } else if (gamepad1.y && canExtend ) {
             // viperSlideMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
             viperSlideMotor.setPower(VIPER_SLIDE_POWER * -1);
-        } else
+        } else {
             viperSlideMotor.setPower(0);
+        }
 
-        // SERVOS
-        // ACTIVE INTAKE - In dpad_down - Out dpad_up
-        // CLAW - Open A Close B
-        // WRIST - Open X - Close Y
-        if (gamepad2.dpad_down)
-            activeIntake.setPower(INTAKE_IN);
-        else if (gamepad2.dpad_up)
-            activeIntake.setPower(INTAKE_OUT);
-        else
-            activeIntake.setPower(0);
+        telemetry.addData("Viper Slide Motor Position", viperSlideMotor.getCurrentPosition());
+    }
 
+    private void controlServos() {
         if (gamepad2.a)
             claw.setPosition(CLAW_OPEN);
         else if (gamepad2.b)
@@ -180,31 +226,6 @@ public class MecanumDriveViperSlide extends OpMode {
             wrist.setPosition(WRIST_OUT);
         else if (gamepad2.y)
             wrist.setPosition(WRIST_MID);
-
-        telemetry.addData("Arm Motor Position", armMotor2.getCurrentPosition() / ARM_TICKS_PER_DEGREE);
-        telemetry.addData("Viper Slide Motor Position", viperSlideMotor.getCurrentPosition());
-        telemetry.update();
-
-    }
-
-    private void mecanumTankDrive() {
-        double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
-        double x = gamepad1.right_stick_x * 1.1; // Counteract imperfect strafing
-        double rx = gamepad1.left_stick_x;
-
-        // Denominator is the largest motor power (absolute value) or 1
-        // This ensures all the powers maintain the same ratio,
-        // but only if at least one is out of the range [-1, 1]
-        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-        double frontLeftPower = (y + x + rx) / denominator;
-        double backLeftPower = (y - x + rx) / denominator;
-        double frontRightPower = (y - x - rx) / denominator;
-        double backRightPower = (y + x - rx) / denominator;
-
-        frontLeftMotor.setPower(frontLeftPower);
-        backLeftMotor.setPower(backLeftPower);
-        frontRightMotor.setPower(frontRightPower);
-        backRightMotor.setPower(backRightPower);
     }
 
 }
