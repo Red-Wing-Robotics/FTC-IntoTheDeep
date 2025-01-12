4d8a0dc943e90b4cec1cda2006ff102cabac02ff
David Tucker <david@davidtucker.net>
2025-01-12 14:11:33 -0500

    Clean up of code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AbstractSparkFunOdometryAutoOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AbstractSparkFunOdometryAutoOpMode.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
@@ -29,17 +28,11 @@ abstract class AbstractSparkFunOdometryAutoOpMode extends LinearOpMode {
     public double MAX_AUTO_TURN  = 0.4;   //  Clip the turn speed to this max value (adjust for your robot)
 
     public SparkFunOTOS.Pose2D startingPosition = new SparkFunOTOS.Pose2D(0, 0, 0);
-
     public SparkFunOTOS.Pose2D offset = new SparkFunOTOS.Pose2D(0, 0, 0);
-
     public DistanceUnit linearUnit = DistanceUnit.INCH;
-
     public AngleUnit angleUnit = AngleUnit.DEGREES;
-
     public double linearScalar = 1.121d;
-
     public double angularScalar = 1.018d;
-
     private ElapsedTime runtime = new ElapsedTime();
 
     // Declare OpMode members for each of the 4 drive motors.
@@ -57,9 +50,7 @@ abstract class AbstractSparkFunOdometryAutoOpMode extends LinearOpMode {
     public void runOpMode() {
 
         // Initialize the hardware variables. Note that the strings used here must correspond
-
         // to the names assigned during the robot configuration step on the DS or RC devices.
-
         leftFrontDrive  = hardwareMap.get(DcMotor.class, "frontLeftMotor");
         leftBackDrive  = hardwareMap.get(DcMotor.class, "backLeftMotor");
         rightFrontDrive = hardwareMap.get(DcMotor.class, "frontRightMotor");
@@ -86,23 +77,14 @@ abstract class AbstractSparkFunOdometryAutoOpMode extends LinearOpMode {
         sleep(1000);
 
         while(!isStarted()) {
-
             // Wait for the game to start (driver presses PLAY)
-
             telemetry.addData("Status", "Initialized");
-
             telemetry.update();
-
         }
 
         waitForStart();
-
         runtime.reset();
-
-
-
         telemetry.addData("Status", "Running");
-
         telemetry.update();
 
         // Execute the Autonomous Commands
@@ -127,9 +109,11 @@ abstract class AbstractSparkFunOdometryAutoOpMode extends LinearOpMode {
         myOtos.setLinearScalar(linearScalar);
         myOtos.setAngularScalar(angularScalar);
         myOtos.calibrateImu();
-        sleep(2000);
+        // Allow sleep for calibration to complete
+        sleep(1000);
         myOtos.resetTracking();
-        sleep(2000);
+        // Allow sleep to reset tracking (likely not needed)
+        sleep(1000);
         myOtos.setPosition(startingPosition);
 
         // Get the hardware and firmware version
@@ -138,120 +122,71 @@ abstract class AbstractSparkFunOdometryAutoOpMode extends LinearOpMode {
         myOtos.getVersionInfo(hwVersion, fwVersion);
 
         // Log out OTOS information
-        telemetry.addLine("OTOS configured! Press start to get position data!");
+        telemetry.addLine("OTOS configured!");
         telemetry.addLine();
         telemetry.addLine(String.format(Locale.US, "OTOS Hardware Version: v%d.%d", hwVersion.major, hwVersion.minor));
         telemetry.addLine(String.format(Locale.US, "OTOS Firmware Version: v%d.%d", fwVersion.major, fwVersion.minor));
         telemetry.update();
     }
 
-
-
     /**
-
      * Move robot to a designated X,Y position and heading
-
      * set the maxTime to have the driving logic timeout after a number of seconds.
-
      */
-
     void otosDrive(double targetX, double targetY, double targetHeading, int maxTime) {
 
         double drive, strafe, turn;
-
         double currentRange, targetRange, initialBearing, targetBearing, xError, yError, yawError;
-
         double opp, adj;
 
-
-
         SparkFunOTOS.Pose2D currentPos = myPosition();
-
         xError = targetX-currentPos.x;
-
         yError = targetY-currentPos.y;
-
         yawError = targetHeading-currentPos.h;
 
-
-
         runtime.reset();
-
-
 
         while(opModeIsActive() && (runtime.milliseconds() < maxTime*1000) &&
 
                 ((Math.abs(xError) > 1.5) || (Math.abs(yError) > 1.5) || (Math.abs(yawError) > 4)) ) {
 
             // Use the speed and turn "gains" to calculate how we want the robot to move.
-
             drive  = Range.clip(xError * SPEED_GAIN, -MAX_AUTO_SPEED, MAX_AUTO_SPEED);
-
             strafe = Range.clip(yError * STRAFE_GAIN, -MAX_AUTO_STRAFE, MAX_AUTO_STRAFE);
-
             turn   = Range.clip(yawError * TURN_GAIN, -MAX_AUTO_TURN, MAX_AUTO_TURN) ;
-
-
 
             telemetry.addData("Auto","Drive %5.2f, Strafe %5.2f, Turn %5.2f ", drive, strafe, turn);
 
             // current x,y swapped due to 90 degree rotation
-
             telemetry.addData("current X coordinate", currentPos.x);
-
             telemetry.addData("current Y coordinate", currentPos.y);
-
             telemetry.addData("current Heading angle", currentPos.h);
-
             telemetry.addData("target X coordinate", targetX);
-
             telemetry.addData("target Y coordinate", targetY);
-
             telemetry.addData("target Heading angle", targetHeading);
-
             telemetry.addData("xError", xError);
-
             telemetry.addData("yError", yError);
-
             telemetry.addData("yawError", yawError);
-
             telemetry.update();
 
-
-
             // Apply desired axes motions to the drivetrain.
-
             moveRobot(drive, strafe, turn);
 
-
-
             // then recalc error
-
             currentPos = myPosition();
-
             xError = targetX-currentPos.x;
-
             yError = targetY-currentPos.y;
-
             yawError = targetHeading-currentPos.h;
-
         }
 
         moveRobot(0,0,0);
-
         currentPos = myPosition();
 
         telemetry.addData("current X coordinate", currentPos.x);
-
         telemetry.addData("current Y coordinate", currentPos.y);
-
         telemetry.addData("current Heading angle", currentPos.h);
-
         telemetry.update();
-
     }
-
-
 
     /* the reported OTOS values are based on sensor orientation, convert to robot centric
         by swapping x and y and changing the sign of the heading
