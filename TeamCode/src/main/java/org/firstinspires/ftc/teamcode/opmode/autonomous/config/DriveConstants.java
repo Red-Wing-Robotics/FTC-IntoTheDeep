a517657cd27e73f359d3f6ead2e0f43d85a66579
David Tucker <david@davidtucker.net>
2025-02-13 10:37:31 -0500

    Adding drive constants file, and will add additional variables eventually.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/autonomous/config/DriveConstants.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.opmode.autonomous.config;
+
+public class DriveConstants {
+    public static final double ROTATE_FUDGE_FACTOR = 2d;
+
+    public static final double DRIVE_POSITION_FUDGE = 1d;
+}
