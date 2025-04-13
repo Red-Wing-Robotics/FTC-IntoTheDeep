2ee8de13889216747f686805f850f95e544b79a4
lukew7375 <lukew7375@gmail.com>
2025-04-13 15:32:12 -0400

    Updating ObservationZoneAuto for dead wheels.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/autonomous/ObservationZoneAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/autonomous/ObservationZoneAuto.java
@@ -30,10 +30,10 @@ public class ObservationZoneAuto extends AbstractAutoOpMode {
         autoDrive(-5, 18,0,4);
         robot.setArmPosition( RobotPosition.ARM_ORIGIN );
         robot.setWristPosition( RobotPosition.WRIST_IN );
-        autoDrive(34, 18, 0, 5);
-        autoDrive(34, 36, 0, 2);
+        autoDrive(35, 18, 0, 5);
+        autoDrive(35, 36, 0, 2);
         autoDrive( 180, RotationDirection.COUNTER_CLOCKWISE, 1);
-        autoDrive(52, -3, 180, 2);
+        autoDrive(58, -3, 180, 2);
         //autoDrive(48, 40, 180, 2);
         //autoDrive(62, 40, 180,2);
         //autoDrive(62, -1, 180, 2);
@@ -41,7 +41,7 @@ public class ObservationZoneAuto extends AbstractAutoOpMode {
         autoDrive(52, 20, 180, 2);
         sleep(1000);
         // Second Specimen
-        autoDrive(56, 20, 180, 2);
+        autoDrive(56, 15, 180, 2);
         robot.setWristPosition( RobotPosition.WRIST_MID );
         robot.setViperSlidePosition( RobotPosition.VIPER_SLIDE_OFF_GROUND );
         sleep(750);
@@ -51,23 +51,23 @@ public class ObservationZoneAuto extends AbstractAutoOpMode {
         autoDrive(-11,10,0,5);
         robot.setArmPosition( RobotPosition.ARM_HANG_SPECIMEN );
         robot.hangSpecimen();
-        autoDrive(-11,33, 0,2);
+        autoDrive(-11,35, 0,2);
         robot.setClawPosition( RobotPosition.CLAW_OPEN );
         robot.setViperSlidePosition( 0 );
         autoDrive(-11,15, 0,5);
         robot.setArmPosition( RobotPosition.ARM_ORIGIN );
         // Third Specimen
-        autoDrive(53, 20, 180, 5);
+        autoDrive(60, 20, 180, 5);
         robot.setViperSlidePosition( RobotPosition.VIPER_SLIDE_OFF_GROUND );
         sleep(1000);
-        autoDrive(56, 18, 180, 5);
+        autoDrive(60, 19, 180, 5);
         sleep(300);
         robot.setClawPosition( RobotPosition.CLAW_CLOSED );
         sleep(200);
         robot.setViperSlidePosition( 0 );
-        autoDrive(-36, 13, 0, 2);
+        autoDrive(-25, 9, 0, 5);
         robot.hangSpecimen();
-        autoDrive(-30, 34, 0, 2);
+        autoDrive(-25, 34, 0, 2);
         robot.setClawPosition( RobotPosition.CLAW_OPEN );
         robot.setViperSlidePosition( 0 );
         autoDrive(-17,24, 0,5);
